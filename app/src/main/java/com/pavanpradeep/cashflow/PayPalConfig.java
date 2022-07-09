dda68bf9d20ca6efe7dfcebfd47ce456cd052955
Pavan <96732038+Pavankulal2002@users.noreply.github.com>
2022-07-09 14:09:28 +0530

    almost finished

--- app/src/main/java/com/pavanpradeep/cashflow/PayPalConfig.java
+++
@@ -1,7 +0,0 @@
-package com.pavanpradeep.cashflow;
-
-public class PayPalConfig {
-
-    public static final String PAYPAL_CLIENT_ID = "AelLT_XpMiGKTlOOyhsEG_lpec3BLUBrdlc1Ow-krfQ4aTeXlH4PaDyT3dEipwVv7GSt5XauYPSruQ2t";
-
-}
